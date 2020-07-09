992baba899927e9e979e4287deb4178b5ba21e62
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-07-09 15:03:55 -0700

    Scrolling bg
    sis the seams tho

--- AntRun/GameScene.swift
+++ AntRun/GameScene.swift
@@ -11,68 +11,70 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var scrollNode: SKNode!
+    let fixedDelta: CFTimeInterval = 1.0 / 60.0 /* 60 FPS */
+    let scrollSpeed: CGFloat = 200
     
     override func sceneDidLoad() {
+        super.sceneDidLoad()
+        
+        setRefs()
 
-        self.lastUpdateTime = 0
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        super.update(currentTime)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        /* Process world scrolling */
+        scrollWorld()
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+    }
+    
+    func setRefs() {
+        // scroll node
+        if let scrollNode = self.childNode(withName: "backgroundScroll") {
+            self.scrollNode = scrollNode
+        } else {
+            print("scrollNode could not be connected properly. u done fucked up")
         }
     }
     
+    func scrollWorld() {
+        
+        /* Scroll World */
+        scrollNode.position.y -= scrollSpeed * CGFloat(fixedDelta)
+
+        /* Loop through scroll layer nodes */
+        for ground in scrollNode.children as! [SKSpriteNode] {
+
+            /* Get ground node position, convert node position to scene space */
+            let groundPosition = scrollNode.convert(ground.position, to: self)
+
+            /* Check if ground sprite has left the scene */
+            if groundPosition.y <= -ground.size.height / 2 {
+
+                /* Reposition ground sprite to the second starting position */
+                let newPosition = CGPoint(x: groundPosition.x, y: ground.size.height*2.5)
+
+                /* Convert new node position back to scroll layer space */
+                ground.position = self.convert(newPosition, to: scrollNode)
+            }
+        }
+    }
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+       
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
@@ -88,23 +90,4 @@ class GameScene: SKScene {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
-    }
 }
